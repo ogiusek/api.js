49fe681910fd93b01eb8e4a2e0919cb2a01b0dae
ogius <kowalewski.olgierd@gmail.com>
2023-05-29 12:52:16 +0200

    Updated init api

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const app = require('./app/app').app;
+const express = require('express');
+const db = require('./db/db').db;
+
+const app = express();
+app.use(express.json());
+
+// req.ip client address
+// req.query data after '?' in url
+// req.body get body of question
+// app .post .get .remove .patch crud
+
+app.get('/sql', (req, res) => {
+    db.all('SELECT * FROM test;', [], (err, rows) => {
+        if (err)
+            return res.sendStatus(400);
+        return res.json({ 'works': rows });
+    });
+});
+
+app.post('/sql', (req, res) => {
+    const body = req.body;
+    if (body.uno && body.dos) {
+        db.run('INSERT INTO test VALUES(?,?);', [body.uno, body.dos]);
+        res.sendStatus(201);
+    } else
+        res.sendStatus(400);
+});
 
 const init = () => { };
-
 app.listen(3000, init);
